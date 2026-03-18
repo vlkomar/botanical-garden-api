3968e471108fa70a01bc9b079d3cb7e24eb8e9f4
vlkawaii <12vladislav8@gmail.com>
2026-03-18 13:35:45 +0200

    feat(zones): use validate on post and get

--- src/routes/zone.routes.ts
+++ src/routes/zone.routes.ts
@@ -1,16 +1,18 @@
 import express from "express";
 
 import * as zoneController from "../controllers/zone.controller.js";
+import { validate } from "../middlewares/validate.js";
+import { createZoneSchema, updateZoneSchema } from "../schemas/zone.schema.js";
 
 const router = express.Router();
 
-router.post("/", zoneController.createZone);
+router.post("/", validate(createZoneSchema), zoneController.createZone);
 
 router.get("/", zoneController.getZones);
 
 router.get("/:id", zoneController.getZoneById);
 
-router.patch("/:id", zoneController.updateZone);
+router.patch("/:id", validate(updateZoneSchema), zoneController.updateZone);
 
 router.delete("/:id", zoneController.deleteZone);
 
